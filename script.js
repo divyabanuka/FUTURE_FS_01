1599163cf8e652e7835ec48743cbd9354e684b50
Divya Banuka <divyabanuka3@gmail.com>
2026-09-12 17:12:24 +0530

    Remove form submission alert and reset functionality

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
 
-    alert("Thank you for contacting me! Your message is being sent.");
-
-    this.reset();
-});
